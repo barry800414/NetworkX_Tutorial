5f85bac77da8d376b152b7883b950410fd1edd48
r02922010 <r02922010@linux11.csie.ntu.edu.tw>
2014-07-19 14:36:35 +0800

    update

--- install_networkx.sh
+++ install_networkx.sh
@@ -1,6 +1,5 @@
-tar -zxvf networkx-1.9.tar.gz
-cd networkx-1.9
-python setup.py build
-python setup.py install --prefix=$HOME
-python3 setup.py build
-python3 setup.py install --prefix=$HOME
+wget https://bootstrap.pypa.io/ez_setup.py -O - | python - --user
+#tar -zxvf networkx-1.9.tar.gz
+#cd networkx-1.9
+#python setup.py build
+#python setup.py install --prefix=$HOME
